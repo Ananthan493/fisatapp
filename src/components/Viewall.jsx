8cadcbf37a48a8f3837b8cae300667a25d53e210
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-11 15:27:58 +0530

    fisat app converted to employee app with integrated api

--- src/components/Viewall.jsx
+++ src/components/Viewall.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 const Viewall = () => {
     const [data,changeData] = useState([])
     const fetchData = () => {
-        axios.get("https://courseapplogix.onrender.com/getdata").then(
+        axios.get("http://localhost:8081/view").then(
             (response) => {
                 changeData(response.data)
 
@@ -22,15 +22,9 @@ const Viewall = () => {
                         <table class="table">
                             <thead>
                                 <tr>
+                                    <th scope="col">Name</th>
                                     <th scope="col">ID</th>
-                                    <th scope="col">FIRST NAME</th>
-                                    <th scope="col">LAST NAME</th>
-                                    <th scope="col">COLLEGE</th>
-                                    <th scope="col">DOB</th>
-                                    <th scope="col">COURSE</th>
-                                    <th scope="col">MOBILE</th>
-                                    <th scope="col">EMAIL</th>
-                                    <th scope="col">ADDRESS</th>
+                                    <th scope="col">Departments</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -39,19 +33,13 @@ const Viewall = () => {
 
                                         (value, index) => {
                                             return <tr>
-                                                <td>{value._idd}</td>
-                                                <td>{value.firstname}</td>
-                                                <td>{value.lastname}</td>
-                                                <td>{value.college}</td>
-                                                <td>{value.dob}</td>
-                                                <td>{value.course}</td>
-                                                <td>{value.mobile}</td>
-                                                <td>{value.email}</td>
-                                                <td>{value.address}</td>
+                                                <td>{value.name}</td>
+                                                <td>{value.id}</td>
+                                                <td>{value.department}</td>
                                             </tr>
 
                                         }
-                                    )
+                                    )   
                                 }
                             </tbody>
                         </table>
